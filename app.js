e8890281b8ebcbb6c871cc39c599b8b372047f8b
Jenil Soni <167244843+jenilsoni01@users.noreply.github.com>
2024-12-31 10:08:18 +0000

    31dec

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-console.log("hello world");
-console.log("hello world");
+const { log } = require('console');
+const { ADDRCONFIG } = require('dns');
+const fs=require('fs');
+// create JSON obj
+const jsonObject={
+    Name: "exe",
+    age: 123,
+    city: "city1"
+}
+
+const jsonString = JSON.stringify(jsonObject,null,2)
+
+const fileDestination = 'app_output.json'
+
+fs.writeFile(fileDestination,jsonString,'utf-8',(err)=>{
+    if(err)console.log("Error writing to file:",err);
+    else console.log('JSON object has been stored in',fileDestination);    
+})
